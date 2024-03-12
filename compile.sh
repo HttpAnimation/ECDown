86a97af7be97b7e1b0c6f4eb8c38c5998575108f
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-11 21:23:19 -0700

    second line

--- compile.sh
+++ compile.sh
@@ -30,10 +30,10 @@ echo "$current_datetime" > "$build_folder/Version.ini"
 # Create README file
 readme_file="$build_folder/README.md"
 echo "# B_ID: $current_datetime"
-echo "Build created at: $current_datetime" > "$readme_file"
+echo "Build created at: $current_datetime" >> "$readme_file"
 echo "This folder contains builds for the server and client programs." >> "$readme_file"
 echo "This is auto message and might work or might not." >> "$readme_file"
-
+echo 
 # Print the path to the build folder
 echo "Builds created at: $build_folder"
 cd $build_folder
