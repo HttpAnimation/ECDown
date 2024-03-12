f28cd42dc39adcca70778025375d3a811fd47431
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-11 21:18:12 -0700

    compile

---
+++ Builds/2024-03-11_21-10-16/Builds/2024-03-11_21-18-00/compile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Create Builds folder if it doesn't exist
+mkdir -p Builds
+
+# Get current date and time
+current_datetime=$(date +'%Y-%m-%d_%H-%M-%S')
+
+# Create a folder with the current date and time
+build_folder="Builds/$current_datetime"
+mkdir -p "$build_folder"
+
+# Compile c scripts into the build folder
+# gcc FILE.c -o "$build_folder/COMPILENAME"
+gcc ECDown.c -o "$build_folder/ECDown" `pkg-config --cflags --libs gtk+-3.0` -lcurl
+
+# Copy files into the build folder
+# cp FILE "$build_folder/FILE"
+cp * "$build_folder"
+
+# Create a Version.ini file with the current date and time
+echo "$current_datetime" > "$build_folder/Version.ini"
+
+# Create README file
+readme_file="$build_folder/README.txt"
+echo "Build created at: $current_datetime" > "$readme_file"
+echo "This folder contains builds for the server and client programs." >> "$readme_file"
+echo "This is auto message and might work or might not." >> "$readme_file"
+
+# Print the path to the build folder
+echo "Builds created at: $build_folder"
+cd $build_folder
