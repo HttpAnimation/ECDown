9443aaa2e3832d11b5818d75817ec2bb3a972907
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-11 21:23:58 -0700

    new lines

--- compile.sh
+++ compile.sh
@@ -34,6 +34,8 @@ echo "Build created at: $current_datetime" >> "$readme_file"
 echo "This folder contains builds for the server and client programs." >> "$readme_file"
 echo "This is auto message and might work or might not." >> "$readme_file"
 echo  "## Files" >> "$readme_file"
+
+
 # Print the path to the build folder
 echo "Builds created at: $build_folder"
 cd $build_folder
